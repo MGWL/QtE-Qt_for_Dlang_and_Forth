f5794304a8b6418ccef96087ce5ba4299a03de91
torabora <torabora08@gmail.com>
2013-11-16 14:00:28 +0300

    Made compile script more readable
    Вынес пути в переменные, добавил ремарку относительно пути к QT4

--- DLL_so/compile.sh
+++ DLL_so/compile.sh
@@ -1,4 +1,13 @@
-g++ -c -pipe -Wall -W -D_REENTRANT -DQT_GUI_LIB -DQT_CORE_LIB -DQT_SHARED -I/usr/lib/qt4/mkspecs/linux-g++ -I. -I/usr/include -I/usr/include/QtCore -I/usr/include/QtGui -I/usr/include/QtScript -fPIC qte.cpp
+#!/bin/bash
+OPTS="-c -pipe -Wall -W -D_REENTRANT -DQT_GUI_LIB -DQT_CORE_LIB -DQT_SHARED -fPIC"
+#Change this path according to your distro features, e.g. in case of ALT Linux IQT4="-I/usr/include/qt4"
+IQT4="-I/usr/include"
+INCL="-I. -I/usr/lib/qt4/mkspecs/linux-g++ $IQT4 $IQT4/QtCore $IQT4/QtGui $IQT4/QtScript $IQT4/QtWebKit"
+LIBS="-lQtCore -lQtGui -lQtScript -lQtWebKit -lpthread"
+
+
+g++ $OPTS $INCL qte.cpp
 moc-qt4 -o moc_qte.cpp qte.h
-g++ -c -pipe -Wall -W -D_REENTRANT -DQT_GUI_LIB -DQT_CORE_LIB -DQT_SHARED -I/usr/lib/qt4/mkspecs/linux-g++ -I. -I/usr/include -I/usr/include/QtCore -I/usr/include/QtGui -I/usr/include/QtScript -fPIC moc_qte.cpp
-g++ -shared -o QtE.so.1.0.0 qte.o moc_qte.o -lQtCore -lQtGui -lQtScript -lpthread
+g++ $OPTS $INCL moc_qte.cpp
+g++ -shared -o QtE.so.1.0.0 qte.o moc_qte.o $LIBS
+
