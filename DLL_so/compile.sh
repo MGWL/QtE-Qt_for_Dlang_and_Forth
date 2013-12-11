91bc545bc30594dfc0e47ff3eea3f2c442146f92
Gennady Mohov <mgwl@mail.ru>
2013-12-11 06:29:18 +0000

    Update compile.sh
    Add Network

--- DLL_so/compile.sh
+++ DLL_so/compile.sh
@@ -2,8 +2,8 @@
 OPTS="-c -pipe -Wall -W -D_REENTRANT -DQT_GUI_LIB -DQT_CORE_LIB -DQT_SHARED -fPIC"
 #Change this path according to your distro features, e.g. in case of ALT Linux IQT4="-I/usr/include/qt4"
 IQT4="-I/usr/include"
-INCL="-I. -I/usr/lib/qt4/mkspecs/linux-g++ $IQT4 $IQT4/QtCore $IQT4/QtGui $IQT4/QtScript $IQT4/QtWebKit"
-LIBS="-lQtCore -lQtGui -lQtScript -lQtWebKit -lpthread"
+INCL="-I. -I/usr/lib/qt4/mkspecs/linux-g++ $IQT4 $IQT4/QtCore $IQT4/QtGui $IQT4/QtScript $IQT4/QtWebKit $IQT4/QtNetwork"
+LIBS="-lQtCore -lQtGui -lQtScript -lQtWebKit -lQtNetwork -lpthread"
 
 
 g++ $OPTS $INCL qte.cpp
